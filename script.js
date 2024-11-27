f3f0bc6d7487edf20439c6525884eab3cf57f51a
githJavier <pedrojavierpablopascual03@gmail.com>
2024-11-27 01:49:11 -0500

    Portafolio acabado con temas claro y oscuro

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById('button');
-const navMenu = document.getElementById('nav-menu-main');    
-const btnTema = document.getElementById('btnTema')
+const navMenu = document.getElementById('nav-menu-main');
+let isLightTheme = false;  
+const btnTema = document.getElementById('btnTema');
+const btnlight = document.getElementById('btn-light');
+const btndark = document.getElementById('btn-dark');
+const headermain = document.getElementById('header-main');
+const imagen = document.getElementById('img-dark');
 const sectionAll = document.querySelectorAll('section[id]');
 const inputName = document.querySelector('#nombre');
 const inputEmail = document.querySelector('#email');
@@ -22,25 +27,20 @@ window.addEventListener('scroll', () => {
 
 /*===== Boton Menu =====*/
 btn.addEventListener('click', function() {
-    // Verificar si el botón tiene la clase 'active'
     if (this.classList.contains('active')) {
         this.classList.remove('active');
         this.classList.add('not-active');
-
-        // Verificar si 'navMenu' tiene alguna de las clases especificadas
         if (navMenu.classList.contains('nav_menu') || navMenu.classList.contains('nav_menu_light')) {
             navMenu.classList.remove('active');
             navMenu.classList.add('not-active');
         }
     }
-    // Si el botón tiene la clase 'not-active'
     else if (this.classList.contains('not-active')) {
         this.classList.add('active');
         this.classList.remove('not-active');
-
         if (navMenu.classList.contains('nav_menu') || navMenu.classList.contains('nav_menu_light')) {
-            navMenu.classList.remove('not-active');
             navMenu.classList.add('active');
+            navMenu.classList.remove('not-active');
         }
     }
 });
@@ -66,6 +66,13 @@ flagsElement.addEventListener('click', (e) => {
 /*===== class active por secciones =====*/
 window.addEventListener('scroll', () => {
     const scrollY = window.pageYOffset;
+
+    if (headermain.classList.contains('abajo') && headermain.classList.contains('header-light')) {
+        imagen.src = "./assets/images/icon-dark-abajo.png"; // Imagen para tema claro
+    } else {
+        imagen.src = "./assets/images/icon-dark.png"; // Imagen por defecto (tema oscuro)
+    }
+
     sectionAll.forEach((current) => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 100;
@@ -73,8 +80,7 @@ window.addEventListener('scroll', () => {
 
         if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
             document.querySelector('nav a[href*=' + sectionId + ']').classList.add('active');
-        }
-        else {
+        } else {
             document.querySelector('nav a[href*=' + sectionId + ']').classList.remove('active');
         }
     });
@@ -98,76 +104,147 @@ document.querySelector('.go-top-container').addEventListener('click', () => {
 });
 
 /*==== Boton y función de tema claro*/
-btnTema.addEventListener('click', function(){
-    document.querySelector('#header-main').classList.remove('header');
-    document.querySelector('#header-main').classList.add('header-light');
+btnTema.addEventListener('click', function () {
+    if (isLightTheme) {
+        // Cambiar a tema oscuro
+        btndark.classList.add('btn-not-active');
+        btnlight.classList.remove('btn-not-active');
 
-    document.querySelector('#nav-menu-main').classList.remove('nav_menu');
-    document.querySelector('#nav-menu-main').classList.remove('active');
-    document.querySelector('#nav-menu-main').classList.add('nav_menu_light');
-    document.querySelector('#nav-menu-main').classList.add('not-active');
+        document.querySelector('#header-main').classList.add('header');
+        document.querySelector('#header-main').classList.remove('header-light');
 
-    document.querySelector('#home').classList.remove('home');
-    document.querySelector('#home').classList.add('home_light');
+        document.querySelector('#nav-menu-main').classList.add('nav_menu');
+        document.querySelector('#nav-menu-main').classList.remove('nav_menu_light');
 
-    document.querySelector('#home_text-1').classList.remove('home_text-1');
-    document.querySelector('#home_text-1').classList.add('home_text-1_light')
+        document.querySelector('#home').classList.add('home');
+        document.querySelector('#home').classList.remove('home_light');
 
-    document.querySelector('#home_text-2').classList.remove('home_text-2');
-    document.querySelector('#home_text-2').classList.add('home_text-2_light');
+        document.querySelector('#home_text-1').classList.add('home_text-1');
+        document.querySelector('#home_text-1').classList.remove('home_text-1_light');
 
-    document.querySelector('#home_text-3').classList.remove('home_text-3');
-    document.querySelector('#home_text-3').classList.add('home_text-3_light');
+        document.querySelector('#home_text-2').classList.add('home_text-2');
+        document.querySelector('#home_text-2').classList.remove('home_text-2_light');
 
-    document.querySelector('#home_text-3').classList.remove('text-type');
-    document.querySelector('#home_text-3').classList.add('text-type_light');
-    
-    document.querySelector('#container-info').classList.remove('container_parrafo-info');
-    document.querySelector('#container-info').classList.add('container_parrafo-info_light');
-    
-    document.querySelector('#btn_cv').classList.remove('cv_button');
-    document.querySelector('#btn_cv').classList.add('cv_button_light');
+        document.querySelector('#home_text-3').classList.add('home_text-3');
+        document.querySelector('#home_text-3').classList.remove('home_text-3_light');
 
-    document.querySelector('#icons-clear').classList.remove('icons-none');
-    document.querySelector('#icons-dark').classList.add('icons-none');
+        document.querySelector('#home_text-3').classList.add('text-type');
+        document.querySelector('#home_text-3').classList.remove('text-type_light');
 
-    document.querySelector('#sobreMi').classList.remove('sobreMi');
-    document.querySelector('#sobreMi').classList.add('sobreMi-light');
+        document.querySelector('#container-info').classList.add('container_parrafo-info');
+        document.querySelector('#container-info').classList.remove('container_parrafo-info_light');
 
-    document.querySelector('#texto_sobreMi').classList.remove('texto_sobreMi');
-    document.querySelector('#texto_sobreMi').classList.add('texto_sobreMi-light');
+        document.querySelector('#btn_cv').classList.add('cv_button');
+        document.querySelector('#btn_cv').classList.remove('cv_button_light');
 
-    document.querySelector('#habilidades').classList.remove('habilidades');
-    document.querySelector('#habilidades').classList.add('habilidades-light');
+        document.querySelector('#icons-clear').classList.add('icons-none');
+        document.querySelector('#icons-dark').classList.remove('icons-none');
 
-    document.querySelector('#go-top-button').classList.remove('go-top-button');
-    document.querySelector('#go-top-button').classList.add('go-top-button-light');
+        document.querySelector('#sobreMi').classList.add('sobreMi');
+        document.querySelector('#sobreMi').classList.remove('sobreMi-light');
 
-    document.querySelector('#formacion').classList.remove('formacion');
-    document.querySelector('#formacion').classList.add('formacion-light');
+        document.querySelector('#texto_sobreMi').classList.add('texto_sobreMi');
+        document.querySelector('#texto_sobreMi').classList.remove('texto_sobreMi-light');
 
-    document.querySelector('#proyectos').classList.remove('proyectos');
-    document.querySelector('#proyectos').classList.add('proyectos-light');
+        document.querySelector('#habilidades').classList.add('habilidades');
+        document.querySelector('#habilidades').classList.remove('habilidades-light');
 
-    document.querySelector('#contacto').classList.remove('contacto');
-    document.querySelector('#contacto').classList.add('contacto-light');
+        document.querySelector('#go-top-button').classList.add('go-top-button');
+        document.querySelector('#go-top-button').classList.remove('go-top-button-light');
 
-    document.querySelector('#footer').classList.remove('footer');
-    document.querySelector('#footer').classList.add('footer-light');
+        document.querySelector('#formacion').classList.add('formacion');
+        document.querySelector('#formacion').classList.remove('formacion-light');
 
-    document.querySelector('#footer-container').classList.remove('container--footer');
-    document.querySelector('#footer-container').classList.add('container--footer-light');
+        document.querySelector('#proyectos').classList.add('proyectos');
+        document.querySelector('#proyectos').classList.remove('proyectos-light');
 
-    document.querySelector('#container-text-footer').classList.remove('container_text--footer');
-    document.querySelector('#container-text-footer').classList.add('container_text--footer-light');
+        document.querySelector('#contacto').classList.add('contacto');
+        document.querySelector('#contacto').classList.remove('contacto-light');
 
-    document.querySelector('#icon-light').classList.add('container-icon-light')
-    document.querySelector('#icon-light').classList.remove('container-icon-none');
-    document.querySelector('#icon-dark').classList.add('container-icon-none');
-    document.querySelector('#icon-dark').classList.remove('container-icon-dark');
+        document.querySelector('#footer').classList.add('footer');
+        document.querySelector('#footer').classList.remove('footer-light');
 
-    btn.classList.remove('');
+        document.querySelector('#footer-container').classList.add('container--footer');
+        document.querySelector('#footer-container').classList.remove('container--footer-light');
 
+        document.querySelector('#container-text-footer').classList.add('container_text--footer');
+        document.querySelector('#container-text-footer').classList.remove('container_text--footer-light');
 
-    
+        document.querySelector('#icon-light').classList.remove('container-icon-light');
+        document.querySelector('#icon-light').classList.add('container-icon-none');
+        document.querySelector('#icon-dark').classList.remove('container-icon-none');
+        document.querySelector('#icon-dark').classList.add('container-icon-dark');
+    } else {
+        // Cambiar a tema claro
+        btndark.classList.remove('btn-not-active');
+        btnlight.classList.add('btn-not-active');
+
+        document.querySelector('#header-main').classList.remove('header');
+        document.querySelector('#header-main').classList.add('header-light');
+
+        document.querySelector('#nav-menu-main').classList.remove('nav_menu');
+        document.querySelector('#nav-menu-main').classList.add('nav_menu_light');
+
+        document.querySelector('#home').classList.remove('home');
+        document.querySelector('#home').classList.add('home_light');
+
+        document.querySelector('#home_text-1').classList.remove('home_text-1');
+        document.querySelector('#home_text-1').classList.add('home_text-1_light');
+
+        document.querySelector('#home_text-2').classList.remove('home_text-2');
+        document.querySelector('#home_text-2').classList.add('home_text-2_light');
+
+        document.querySelector('#home_text-3').classList.remove('home_text-3');
+        document.querySelector('#home_text-3').classList.add('home_text-3_light');
+
+        document.querySelector('#home_text-3').classList.remove('text-type');
+        document.querySelector('#home_text-3').classList.add('text-type_light');
+
+        document.querySelector('#container-info').classList.remove('container_parrafo-info');
+        document.querySelector('#container-info').classList.add('container_parrafo-info_light');
+
+        document.querySelector('#btn_cv').classList.remove('cv_button');
+        document.querySelector('#btn_cv').classList.add('cv_button_light');
+
+        document.querySelector('#icons-clear').classList.remove('icons-none');
+        document.querySelector('#icons-dark').classList.add('icons-none');
+
+        document.querySelector('#sobreMi').classList.remove('sobreMi');
+        document.querySelector('#sobreMi').classList.add('sobreMi-light');
+
+        document.querySelector('#texto_sobreMi').classList.remove('texto_sobreMi');
+        document.querySelector('#texto_sobreMi').classList.add('texto_sobreMi-light');
+
+        document.querySelector('#habilidades').classList.remove('habilidades');
+        document.querySelector('#habilidades').classList.add('habilidades-light');
+
+        document.querySelector('#go-top-button').classList.remove('go-top-button');
+        document.querySelector('#go-top-button').classList.add('go-top-button-light');
+        
+        document.querySelector('#formacion').classList.remove('formacion');
+        document.querySelector('#formacion').classList.add('formacion-light');
+
+        document.querySelector('#proyectos').classList.remove('proyectos');
+        document.querySelector('#proyectos').classList.add('proyectos-light');
+
+        document.querySelector('#contacto').classList.remove('contacto');
+        document.querySelector('#contacto').classList.add('contacto-light');
+
+        document.querySelector('#footer').classList.remove('footer');
+        document.querySelector('#footer').classList.add('footer-light');
+
+        document.querySelector('#footer-container').classList.remove('container--footer');
+        document.querySelector('#footer-container').classList.add('container--footer-light');
+
+        document.querySelector('#container-text-footer').classList.remove('container_text--footer');
+        document.querySelector('#container-text-footer').classList.add('container_text--footer-light');
+
+        document.querySelector('#icon-light').classList.add('container-icon-light');
+        document.querySelector('#icon-light').classList.remove('container-icon-none');
+        document.querySelector('#icon-dark').classList.add('container-icon-none');
+        document.querySelector('#icon-dark').classList.remove('container-icon-dark');
+        
+    }
+
+    isLightTheme = !isLightTheme;
 });
